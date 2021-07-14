3bc9d2abb55fea6e479c3dcc81970cf2c60fefc0
Wang Jun Hong <e0544005@u.nus.edu>
2021-07-14 16:00:30 +0800

    change loading

--- client/src/Loading.js
+++ client/src/Loading.js
@@ -1,9 +1,5 @@
 import React from "react";
 const Loading = () => {
-  return (
-    <div className="loadingBG">
-      <span className="loadingText">Loading page...</span>
-    </div>
-  );
+  return <div className="loadingBG"></div>;
 };
 export default Loading;
